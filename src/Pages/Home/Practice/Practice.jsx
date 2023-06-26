c8239ec5f90716948df0e48b4deee9eb26d70712
unknown <liptonbarua274@gmail.com>
2023-06-26 13:23:20 +0600

    Project

--- src/Pages/Home/Practice/Practice.jsx
+++ src/Pages/Home/Practice/Practice.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function Clock() {
-  const [currentTime, setCurrentTime] = useState(new Date());
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+const Practice = () => {
   return (
     <div>
-      <h1>React Clock</h1>
-      <p>Current time: {currentTime.toLocaleTimeString()}</p>
+      
     </div>
   );
-}
+};
 
-export default Clock;
+export default Practice;
