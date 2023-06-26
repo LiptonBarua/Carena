9d899d4b95422045f9e8b78a6edc09625162ec57
unknown <liptonbarua274@gmail.com>
2023-06-26 21:53:40 +0600

    project

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
             <section className="pt-14 bg-[black] bg-opacity-90" >
                 <div className="flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
                     <div className="text-white flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-3xl font-bold leading-none sm:text-5xl">Second Hand Mobiles For Sale !!!</h1>
+                        <h1 className="text-3xl font-bold leading-none sm:text-5xl">Second Hand Car For Sale !!!</h1>
                         <p className="md:text-justify mt-6 mb-8 text-lg sm:mb-12">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                             <button className="text-[white] border border-[#0a8803] hover:bg-[black] hover:border-white  active:bg-[#0a8803] bg-[#0a8803] font-semibold text-lg px-8 py-3 outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" style={{transitionDuration: `1s`, backgroundColor: isHovering ? '' : colors[0]?.color,  borderColor: isHovering ? 'white' : colors[0]?.color,}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Starting</button>
--- src/Pages/Home/SmallBanner/SmallBanner.jsx
+++ src/Pages/Home/SmallBanner/SmallBanner.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FiCircle } from "react-icons/fi";
+import { ShareContext } from '../../../ShareProvider/ShareProvider';
 
 const SmallBanner = () => {
+    const{colors}=useContext(ShareContext)
+
     return (
-        <div className='hidden lg:flex mt-36'>
-            <img className='w-full h-full' src="https://i.ibb.co/7RS0HnV/Black-White-Modern-Car-Wash-Detailing-Twitter-Header.png" alt="" />
+        <div className='hidden mt-52 h-72 w-full lg:flex' style={{backgroundImage: `url(https://i.ibb.co/qdLt0Ms/Black-White-Orange-Modern-Car-Sale-Billboard-Web-Ad.png)`, backgroundRepeat: `no-repeat`, backgroundPosition: `center`, backgroundSize: `cover`}}>
+            {/* <img className='w-full h-full' src="https://i.ibb.co/qdLt0Ms/Black-White-Orange-Modern-Car-Sale-Billboard-Web-Ad.png" alt="" /> */}
+<div className=''>
+<div className='flex font-bold text-[#0a8803] pl-8 mt-3' style={{color: colors[0]?.color}}>
+    <FiCircle className='mx-2'></FiCircle>
+    <FiCircle></FiCircle>
+    <FiCircle className='mx-2'></FiCircle>
+    <FiCircle></FiCircle>
+</div>
+          <div className='mt-10'>
+          <div className='pl-8 text-[#0a8803]' style={{color: colors[0]?.color}}>
+           <h1 className='text-6xl font-bold uppercase'>car resale</h1>
+           <div className='flex items-center'>
+           <p className='uppercase text-white'>price starting from:</p>
+           <button className=" px-4 m-2 text-sm font-bold transition-colors duration-150 bg-white text-black rounded-lg">$520000</button>
+            </div>
+           </div>
+           <button className="h-10 px-7 transition-colors text-white bg-[#0a8803]" style={{backgroundColor: colors[0]?.color}}>BOOK NOW</button>
+           <div className='pl-8 mt-2 flex justify-between text-[#0a8803]' style={{color: colors[0]?.color}}>
+            <small>01538278521</small>
+            <small>lliptonbarua274@gmail.com</small>
+           </div>
+          </div>
+</div>
         </div>
     );
 };
