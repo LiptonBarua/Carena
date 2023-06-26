e5445ec552d9807c9dca7230963dd2facd74937c
unknown <liptonbarua274@gmail.com>
2023-06-26 13:19:43 +0600

    project

--- src/Pages/Home/TimeZone/TimeZone.jsx
+++ src/Pages/Home/TimeZone/TimeZone.jsx
@@ -52,7 +52,7 @@ const TimeZone = () => {
      <div>
      <img src="https://i.ibb.co/NSqyvK0/618-XO0-Z6-S7-L-AC-UF350-350-QL80-removebg-preview.png" alt="" className="w-32 h-32 mx-auto rounded-full" />
      <div className='p-3 mt-3 rounded-md shadow-lg shadow-[white]' style={{boxShadow: colors[0]?.color}}>
-     <p className="text-2xl lg:text-3xl font-bold">{currentDateTime.toLocaleTimeString()}</p>
+     <p className="text-2xl lg:text-3xl font-bold">{formatTime(currentDateTime)}</p>
       <p>{formatDay(currentDateTime)} {formatDate(currentDateTime)}</p>
       <p></p>
      </div>
