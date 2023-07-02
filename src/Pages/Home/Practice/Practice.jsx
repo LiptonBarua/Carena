45179c80b22bfd02de9f9ffa780ddb4306ebdb5f
unknown <liptonbarua274@gmail.com>
2023-07-02 22:06:30 +0600

    project

--- src/Pages/Home/Practice/Practice.jsx
+++ src/Pages/Home/Practice/Practice.jsx
@@ -1,14 +1,26 @@
+// import React from 'react';
+
+// const Practice = () => {
+//   return (
+//     <div className='relative h-96 mb-5 bg-black flex'>
+//       <div className='bg-black w-96 h-full'>
+
+//       </div>
+//       <div className='w-2/3 absolute right-0 h-full bg-[green]' style={{clipPath: `polygon(46% 0, 100% 0%, 100% 100%, 0 100%)`}}>
+
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Practice;
+
 import React from 'react';
 
 const Practice = () => {
   return (
-    <div className='relative h-96 mb-5 bg-black flex'>
-      {/* <div className='bg-black w-96 h-full'>
-
-      </div>
-      <div className='w-2/3 absolute right-0 h-full bg-[green]' style={{clipPath: `polygon(46% 0, 100% 0%, 100% 100%, 0 100%)`}}>
-
-      </div> */}
+    <div>
+      
     </div>
   );
 };
