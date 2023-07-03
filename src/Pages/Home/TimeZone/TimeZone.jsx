3c373b62ae2c3222f3a4947b937b7583bef8a993
unknown <liptonbarua274@gmail.com>
2023-07-03 10:41:14 +0600

    project

--- src/Pages/Home/TimeZone/TimeZone.jsx
+++ src/Pages/Home/TimeZone/TimeZone.jsx
@@ -54,17 +54,30 @@ const TimeZone = () => {
   },1000)
 
   return (
-   <div style={{backgroundImage: `url(https://w0.peakpx.com/wallpaper/20/744/HD-wallpaper-black-pattern-black-design-modern.jpg)`}}>
-     <div className='flex justify-center py-8 text-white'>
-     <div>
-     <Clock className='bg-info text-black rounded-full' value={currentDateTime} renderNumbers={true}></Clock>
-     <div className=' rounded-md shadow-lg px-3 mt-3 shadow-[#ffffff]' style={{boxShadow: colors[0]?.color}}>
-     <p className="text-2xl lg:text-3xl font-bold">{formatTime(currentDateTime)}</p>
-      <p>{formatDay(currentDateTime)} {formatDate(currentDateTime)}</p>
-     </div>
-     </div>
+  //  <div style={{backgroundImage: `url(https://w0.peakpx.com/wallpaper/20/744/HD-wallpaper-black-pattern-black-design-modern.jpg)`}}>
+  //    <div className=' py-8 text-white'>
+  //    <div className="mx-auto">
+  //    <Clock className='bg-info text-black rounded-full' value={currentDateTime} renderNumbers={true}></Clock>
+  //    <div className='rounded-md px-3 mt-3 shadow-[#ffffff]' style={{boxShadow: colors[0]?.color}}>
+  //    <p className="text-2xl lg:text-3xl font-bold">{formatTime(currentDateTime)}</p>
+  //     <p>{formatDay(currentDateTime)} {formatDate(currentDateTime)}</p>
+  //    </div>
+  //    </div>
+  //   </div>
+  //  </div>
+
+  <div>
+    <section style={{backgroundImage: `url(https://w0.peakpx.com/wallpaper/20/744/HD-wallpaper-black-pattern-black-design-modern.jpg)`}}>
+	<div className="container flex flex-col items-center justify-center p-4 mx-auto">
+    <Clock className='bg-info text-black rounded-full' value={currentDateTime} renderNumbers={true}></Clock>
+		<div className="text-white mt-3 shadow-[#ffffff] shadow-lg px-3">
+    <p className="max-w-2xl text-center text-2xl lg:text-3xl font-bold">{formatTime(currentDateTime)}</p>
+    <p className="max-w-2xl text-center">{formatDay(currentDateTime)} {formatDate(currentDateTime)}</p>
     </div>
-   </div>
+
+	</div>
+</section>
+  </div>
   );
 };
 
